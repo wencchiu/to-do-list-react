8284e44a16dde9f398584f6965c87fb3b0643dca
WenChiu <wen2214@gmail.com>
2020-02-20 02:07:28 +0800

    Updates

---
+++ precache-manifest.27e1907c17074a6250b683fbc651c5c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597bc2c6d66eda5d86deb723d695d657",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "d07696439ad1e06fa4c6",
+    "url": "/to-do-list-react/static/css/main.2470cb26.chunk.css"
+  },
+  {
+    "revision": "d86e89a527db5a69b9cd",
+    "url": "/to-do-list-react/static/js/2.19c988ac.chunk.js"
+  },
+  {
+    "revision": "d07696439ad1e06fa4c6",
+    "url": "/to-do-list-react/static/js/main.6e311863.chunk.js"
+  },
+  {
+    "revision": "7daeee6fb15381a1a7de",
+    "url": "/to-do-list-react/static/js/runtime~main.68bfc360.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-list-react/precache-manifest.65ab2c73c16e99e115906d5805cb0935.js"
+  "/to-do-list-react/precache-manifest.27e1907c17074a6250b683fbc651c5c5.js"
 );
 
 self.addEventListener('message', (event) => {
